c85af4ea1c59f993c15d743d8c1491ea8145c1ae
r0lith <96643526+r0lith@users.noreply.github.com>
2025-02-26 00:36:01 +0530

    Create chk.js

---
+++ plugins/chk.js
@@ -0,0 +1,19 @@
+let handler = async (m, { conn, usedPrefix, command }) => {
+    
+    m.reply("🚀 Listening for WebSocket messages related to community removals...");
+
+    conn.ws.on('frame', (frame) => {
+        console.log("🛰️ WebSocket Frame Detected:");
+        console.log(JSON.stringify(frame, null, 2));
+    });
+
+}
+
+handler.help = ['chk'];
+handler.tags = ['group'];
+handler.command = ['chk']; 
+handler.admin = true;
+handler.group = true;
+handler.botAdmin = true;
+
+export default handler;
