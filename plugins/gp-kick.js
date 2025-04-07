cba7977f8bd4a9e10654edf09c1bf8c1b443a23a
r0lith <96643526+r0lith@users.noreply.github.com>
2025-04-07 15:48:35 +0530

    Update gp-kick.js

--- plugins/gp-kick.js
+++ plugins/gp-kick.js
@@ -1,8 +1,15 @@
-
 let handler = async (m, { conn, usedPrefix, command }) => {
 	
 if (!m.mentionedJid[0] && !m.quoted) return m.reply(`✳️ ${mssg.useCmd}\n\n*${usedPrefix + command}* @tag`) 
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
+
+// Check if the user is the owner
+let userNumber = user.split('@')[0];
+let userName = conn.getName(user);
+  if (userNumber === '919737825303' || userName === 'Rolith') {
+    return conn.sendMessage(m.chat, { text: "He's my owner, you idiot." });
+  }
+
 if (conn.user.jid.includes(user)) return m.reply(`✳️ No puedo hacer un auto kick`)
 
 await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
