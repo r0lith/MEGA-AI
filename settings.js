10e5e80a965e76bc157229f6f4bffa9d3eeb6d78
r0lith <96643526+r0lith@users.noreply.github.com>
2025-12-15 15:20:59 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Riruru',
   botName: "Riruru",
   botOwner: 'Rolith', // Your name
-  ownerNumber: '12482586072', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '919737825303', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   listv: ['¤','☞','⎆','⫹⫺','➩','➢','➣','➤','✦','✧','○','◇'],
   commandMode: "public",
